9c658c6e6338051802209ebcaa70433edac1d8e2
Caleb335 <belac335@gmail.com>
2020-02-06 23:02:30 +0100

    added TodoItems

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
-
+import TodoItems from "../Components/TodoItems";
 class TodoList extends Component {
+    constructor(prop) {
+        super(prop);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+    addItem(e) {
+        if(this._inputElement.value !== "") {
+            let newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+            this._inputElement.value = "";
+        }
+
+        console.log(this.state.items);
+
+        e.preventDefault();
+    }
     render() {
         return (
             <div className="todoListContainer">
                 <div className="header">
-                    <form>
-                        <input placeholder="What do you wanna do?"></input>
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a} placeholder="What do you wanna do?"></input>
                         <button type="submit">Add</button>
                     </form>
                 </div>
+                <TodoItems entries={this.state.items} />
             </div>
         );
     }
