f284582d69d915994cc6f0083b3f10e6129dccee
Caleb335 <belac335@gmail.com>
2020-11-06 15:08:08 +0100

    add base components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import TodoList from "./Components/TodoList";
-import "./list-icon.png";
+import React from "react"
+import ReactDOM from "react-dom"
+import App from './app'
 
-let destination = document.getElementById("container");
-
-ReactDOM.render(
-    <div>
-        <TodoList />
-    </div>,
-    destination
-);
+ReactDOM.render(<App />, document.querySelector("#root"))
